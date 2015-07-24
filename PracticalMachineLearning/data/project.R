5cbc2f60ffee592d6bcc2a42b733a8f6c859ecd9
Anton Kapliy <kapliy@gmail.com>
2015-07-23 20:01:33 -0400

    updates

--- PracticalMachineLearning/data/project.R
+++ PracticalMachineLearning/data/project.R
@@ -4,9 +4,10 @@
 
 library('caret')   # learning
 library('rattle')  # plotting of trees
+library('forecast')
 # parallel computing to utilize multiple cores
-library(doParallel)
-registerDoParallel(cores=2)
+#library(doParallel)
+#registerDoParallel(cores=2)
 
 ##############################################
 # DATA LOAD AND PRE-PROCESSING
@@ -34,43 +35,47 @@ print(cat('Dropped', length(cols_novar), 'columns (reason: zero variance):', col
 cols <- cols[!(cols %in% cols_novar)]
 
 # 3. drop bad columns from both the training and testing (aka benchmark) datasets
-ftrain <- pml_training[, colnames(pml_training) %in% cols]
-fbench <- pml_testing[, colnames(pml_testing) %in% c(cols, 'problem_id')]
-
-# 4. separate numeric and factor columns (for later use)
-cols_numeric_bool <- sapply(cols, function(col){is.numeric(ftrain[,col])})
-cols_numeric <- cols[cols_numeric_bool == T]
-cols_factor <- cols[-cols_numeric_bool == F]
-
+df <- pml_training[, colnames(pml_training) %in% cols]
+df_bench <- pml_testing[, colnames(pml_testing) %in% c(cols, 'problem_id')]
 
 ##############################################
 # DATA VISUALIZATION
 ##############################################
 
 # 1. Distribution of dependent variables
-qplot(ftrain$classe, geom="histogram", main='Distribution of dependent variable', xlab='classe', ylab='Counts')
+qplot(df$classe, geom="histogram", main='Distribution of dependent variable', xlab='classe', ylab='Counts')
 
 # Use PCA analysis to extract highest-variance predictors so that we can visualize them
 # df_pca <- preProcess(ftrain[, cols_numeric], method="pca",thresh=0.99)
 # TODO - consider applying PCA: reduce down to 75 vars!
 
+##############################################
+# SPLIT DATASET INTO TRAINING/VALIDATION/TESTING
+##############################################
+
 # split full dataset (ftrain) into train/val/test using 60-20-20 fractions
 set.seed(1)
-p6 <- createDataPartition(ftrain$classe, p=0.6, list=F)
-train <- ftrain[p6, ]  # 60%
-tv <- ftrain[-p6, ]    # 40%
-p5 <- createDataPartition(tv$classe, p=0.5, list=F)
-val <- tv[p5, ]       # 0.5*40% = 20%
-test <- tv[-p5, ]     # 0.5*40% = 20%
+p6 <- createDataPartition(df$classe, p=0.6, list=F)
+df_train <- df[p6, ]  # 60%
+tmp <- df[-p6, ]    # 40%
+p5 <- createDataPartition(tmp$classe, p=0.5, list=F)
+df_val <- tmp[p5, ]       # 0.5*40% = 20%
+df_test <- tmp[-p5, ]     # 0.5*40% = 20%
 
+##############################################
+# TRAIN SEVERAL MODELS ON TRAINING SET
+# NOTE: because training is slow, we create functions
+#       that can cache and quickly retrieved a trained
+#       model from a .rds file on disk
+##############################################
 
-get_model_01 <- function(save_mode = F) {
-    # random forest
-    name <- 'model.v01.rf.all.rds'
+# 1. rf (random forest) without extra pre-processing
+get_mod1 <- function(save_mode = F) {
+    name <- 'model.v01.rf.rds'
     if (save_mode == T) {
         set.seed(1)
-        mod <- train(classe~., data=train, method='rf', # preProcess=c("center", "scale", "pca"),
-                     allowParallel=T, trControl = trainControl(method = "cv", number=10))
+        mod <- train(classe~., data=df_train, method='rf',
+                     trControl=trainControl(method="cv", number=10))
         saveRDS(mod, name)
     } else {
         mod <- readRDS(name)
@@ -78,72 +83,61 @@ get_model_01 <- function(save_mode = F) {
     mod
 }
 
-get_model_02 <- function(save_mode = F) {
-    # decision tree
-    name <- 'model.v02.rpart.all.rds'
+# 2. rf (random forest) with center-scale-PCA preprocessing
+get_mod2 <- function(save_mode = F) {
+  name <- 'model.v02.rf_pca.rds'
+  if (save_mode == T) {
+    set.seed(1)
+    mod <- train(classe~., data=df_train, method='rf',
+                 preProcess=c("center", "scale", "pca"),
+                 trControl=trainControl(method="cv", number=10))
+    saveRDS(mod, name)
+  } else {
+    mod <- readRDS(name)
+  }
+  mod
+}
+
+# 3. gbm (boosted decision tree)
+get_mod3 <- function(save_mode = F) {
+    name <- 'model.v03.gbm.rds'
     if (save_mode == T) {
-        set.seed(1)
-        mod <- train(classe~., data=train, method='rpart', # preProcess=c("center", "scale", "pca"),
-                     allowParallel=T)
+        set.seed(10)
+        mod <- train(classe~., data=df_train, method='gbm', verbose=F)
         saveRDS(mod, name)
     } else {
         mod <- readRDS(name)
     }
-    #fancyRpartPlot(mod$finalModel)
     mod
 }
 
-get_model_03 <- function(save_mode = F) {
-    # boosted decision tree
-    name <- 'model.v03.gbm.all.rds'
-    if (save_mode == T) {
-        set.seed(1)
-        mod <- train(classe~., data=train, method='gbm', # preProcess=c("center", "scale", "pca"),
-                     allowParallel=T, trControl = trainControl(method = "cv", number=5))
-        saveRDS(mod, name)
-    } else {
-        mod <- readRDS(name)
-    }
-    mod
-}
-
+# 5. function to train and save all models to disk
 train_all_models <- function() {
-    get_model_01(save_mode=T)
-    get_model_02(save_mode=T)
-    get_model_03(save_mode=T)
+    get_mod3(save_mode=T)
+    get_mod2(save_mode=T)
+    get_mod1(save_mode=T)
 }
 
 
-combine_models <- function() {
-    # train combiner
-    pred1 <- predict(mod1,newdata=training);
-    pred2 <- predict(mod2,newdata=training)
-    pred3 <- predict(mod3,newdata=training)
-    predDF <- data.frame(pred1,pred2,pred3,diagnosis=training$diagnosis)
-    combModFit <- train(diagnosis~.,method="rf",data=predDF)
-    
-    # use combiner
-    pred1 <- predict(mod1,newdata=testing);
-    pred2 <- predict(mod2,newdata=testing)
-    pred3 <- predict(mod3,newdata=testing)
-    predDF <- data.frame(pred1,pred2,pred3,diagnosis=testing$diagnosis)
-    combPred <- predict(combModFit, newdata=predDF)
-}
+##############################################
+# CHOOSE THE BEST MODEL USING VALIDATION DATASET
+##############################################
+# train_all_models()
 
-plot_tree <- function(mod) {
-    fancyRpartPlot(mod$finalModel)
-    #qplot(predict(mod,test),wage,data=testing)
-    
-    #pred1 <- predict(mod1,testing); pred2 <- predict(mod2,testing)
-    #qplot(pred1,pred2,colour=wage,data=testing)
-}
+mod1 <- get_mod1()
+mod2 <- get_mod2()
+mod3 <- get_mod3()
 
-get_accuracy <- function() {
-    library(forecast)
-    accuracy(prediction, testing$CompressiveStrength)
-    
-}
 
+accuracy(predict(mod1, df_val), df_val$classe)
+
+## collect resamples
+## results <- resamples(list(LVQ=modelLvq, GBM=modelGbm, SVM=modelSvm))
+
+
+##############################################
+# EVALUATE ACCURACY ON TEST DATASET
+##############################################
 # train_all_models()
 
 mod1 <- get_model_01()
@@ -152,16 +146,9 @@ mod1 <- get_model_01()
 # Out-of-sample error
 confusionMatrix(predict(mod1, val), val$classe)
 
-# Prediction
-pred <- predict(mod1, newdata=fbench)
+##############################################
+# PREDICT 20 BENCHMARK CASES
+##############################################
+pred <- predict(mod1, newdata=df_bench)
 print(pred)
 
-# evaluate
-
-# combining predictors
-#predDF <- data.frame(pred1,pred2,wage=testing$wage)
-#combModFit <- train(wage ~.,method="gam",data=predDF)
-#combPred <- predict(combModFit,predDF)
-#pred1V <- predict(mod1,validation); pred2V <- predict(mod2,validation)
-#predVDF <- data.frame(pred1=pred1V,pred2=pred2V)
-#combPredV <- predict(combModFit,predVDF)
